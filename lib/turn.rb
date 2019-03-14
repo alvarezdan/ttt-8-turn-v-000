55345608aec414a97cc448dfd581b4d0ca545b17
Daniel Alvarez <dnielalvrz@gmail.com>
2019-03-14 17:55:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,13 +15,7 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  taken = nil
-    if(board[index] == " " || board[index] == "" || board[index] == nil)
-      taken = false
-    else
-      taken = true
-    end
-  taken
+  board[index] != " "
 end
 
 def move(board, index, player_char = "X")
@@ -32,7 +26,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip              #get user input
   index = input_to_index(input)   #assign index equal to converted user input
-    if valid_move?(board, index)  
+    if valid_move?(board, index)
       move(board, index)
       display_board(board)
     else
